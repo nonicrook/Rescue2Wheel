855c9c1e7d0cf44541907c2a1f4ee54417b37587
Noni Crook <97472538+nonicrook@users.noreply.github.com>
2022-07-05 11:20:44 -0400

    Created Dispatch class
    Created a Dispatch class.

---
+++ Dispatch.java
@@ -0,0 +1,19 @@
+// Motorcycle Towing Application
+public class Dispatch {
+
+  // Instance Variables
+  private int dispatchId
+  private LocalDateTime startDateTime
+  private LocalDateTime endDateTime
+  private String startAddress
+  private String endAddress
+  private String towOperator
+  
+  // Constructor
+  
+  // Getters and Setters
+  
+  // Methods
+  
+  
+}
