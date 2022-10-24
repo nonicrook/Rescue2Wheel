bdcb81b8cedaa20af7cdaf198ceb130ebf3b882a
Noni Crook <97472538+nonicrook@users.noreply.github.com>
2022-10-24 16:01:25 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,1 +1,18 @@
 //Motorcycle Towing App - Customer
+public class Customer {
+  
+  long custId;
+  String fullName;
+  String email;
+  String phone;
+  
+  public Customer(long custId, String fullName, String email, String phone) {
+    custId = this.custId;
+    fullName = this.fullName;
+    email = this.email;
+    phone = this.phone;
+  }
+  
+  
+  
+}
