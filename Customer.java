5d54e9da4e5ae5c875214b8a1f7379a5f6f757b1
Noni Crook <97472538+nonicrook@users.noreply.github.com>
2022-10-24 16:57:47 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -7,12 +7,21 @@ public class Customer {
   String phone;
   
   public Customer(long custId, String fullName, String email, String phone) {
-    custId = this.custId;
-    fullName = this.fullName;
-    email = this.email;
-    phone = this.phone;
+    this.custId = custId;
+    this.fullName = fullName;
+    this.email = email;
+    this.phone = phone;
   }
   
+  // Getters and Setters
+  public long getCustId() {return custId;}
+  public String getFullName() {return fullName;}
+  public String getEmail() {return email;}
+  public String getPhone() {return phone;}
   
+  public void setCustId(long custId) {this.custId = custId;}
+  public void setFullName(String fullName) {this.fullName = fullName;}
+  public void setEmail(String email) {this.email = email;}
+  public void setPhone(String phone) {this.phone = phone;}  
   
 }
