33908c0462cbfc023d592f5ea7cd67ea98db8f11
Noni Crook <97472538+nonicrook@users.noreply.github.com>
2024-02-06 18:07:53 -0500

    update getName in Customer.java

--- Customer.java
+++ Customer.java
@@ -3,26 +3,36 @@ public class Customer {
 
     // Instance Variables
     private int customerId;
-    private String customerName;
-    private String address;
+    private String name;
     private String emailAddress;
     private String phoneNumber;
 
     //  Constructor
-    public Customer(int customerId, String customerName, String address, String emailAddress, String phoneNumber) {
+    public Customer(int customerId, String name, String emailAddress, String phoneNumber) {
         this.customerId = customerId;
-        this.customerName = customerName;
-        this.address = address;
+        this.name = name;
         this.emailAddress = emailAddress;
         this.phoneNumber = phoneNumber;
     }
 
     // Getters and Setters
+    public long getCustId() {return custId;}
+    // public String getName() {return name;}
+    public String getEmail() {return email;}
+    public String getPhone() {return phone;}
     
+    public void setCustId(long custId) {this.custId = custId;}
+    public void setName(String name) {this.name = name;}
+    public void setEmail(String email) {this.email = email;}
+    public void setPhone(String phone) {this.phone = phone;}
 
     // Methods
-    public String getCustomerName (String firstName, String lastName) {
-        customerName = firstName + LastName;
-        return customerName;
+    public String getName (String firstName, String lastName, String bizName) {
+        if(bizName.isEmpty()){
+            name = firstName + " " + lastName;
+        }else{
+            name = firstName + " " + lastName + "," + bizName;
+        }
+        return name;
   }
 }
